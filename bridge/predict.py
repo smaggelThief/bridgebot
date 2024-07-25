e2b4f19bf6ef63a1c6c14152f1fe11f31cf94011
Zaden Lockwood <68566195+smaggelThief@users.noreply.github.com>
2024-07-25 00:21:05 -0400

    works

--- bridge/predict.py
+++ bridge/predict.py
@@ -4,12 +4,14 @@ import sys
 import urllib.request
 from urllib.request import Request, urlopen
 
+
 # Load a model
 model = YOLO("best.pt")  # pretrained YOLOv8n model
 
 # Run batched inference on a list of images
 # results = model(source="im2.jpg", imgsz=800, conf=0.01, max_det=40)  # return a list of Results objects
 link = sys.argv[1]
+print(link)
 req = Request(
     url=link, 
     headers={'User-Agent': 'Mozilla/5.0'}
@@ -28,19 +30,24 @@ for x in f:
     data = x.split()
     if float(data[5]) > max_con:
         max_con = float(data[5])
-print("This photo is ", (max_con * 100),"%", " bridge")
 
 f.close()
 os.remove("im1.jpg")
 os.remove("runs/detect/predict/labels/im1.txt")
 os.rmdir("runs/detect/predict/labels")
 os.rmdir("runs/detect/predict")
-# Process results list
-# for result in results:
-#     boxes = result.boxes  # Boxes object for bounding box outputs
-#     masks = result.masks  # Masks object for segmentation masks outputs
-#     keypoints = result.keypoints  # Keypoints object for pose outputs
-#     probs = result.probs  # Probs object for classification outputs
-#     obb = result.obb  # Oriented boxes object for OBB outputs
-#     result.show()  # display to screen
-#     result.save(filename="result.jpg")  # save to disk
+
+string_out = str("This photo is " + str(max_con * 100) + "%" + " bridge")
+f = open("message.txt", "w")
+f.write(string_out)
+f.close()
+
+    # Process results list
+    # for result in results:
+    #     boxes = result.boxes  # Boxes object for bounding box outputs
+    #     masks = result.masks  # Masks object for segmentation masks outputs
+    #     keypoints = result.keypoints  # Keypoints object for pose outputs
+    #     probs = result.probs  # Probs object for classification outputs
+    #     obb = result.obb  # Oriented boxes object for OBB outputs
+    #     result.show()  # display to screen
+    #     result.save(filename="result.jpg")  # save to disk
