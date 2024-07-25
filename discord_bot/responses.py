1e16db8766a6916f82a34d85b980eaf1ea08af8f
Zaden Lockwood <68566195+smaggelThief@users.noreply.github.com>
2024-07-24 23:35:10 -0400

    integrate bot to ai

--- discord_bot/responses.py
+++ discord_bot/responses.py
@@ -1,20 +1,11 @@
 from random import choice, randint
+import os
 
 
 def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
+    lowered: str = user_input
 
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
-        return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
-    elif 'bye' in lowered:
-        return 'See you!'
-    elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
-    else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
+    if lowered.split()[0] == "/bridge":
+        string = "python3 ../bridge/predict.py " + "\""+ lowered.split()[1] + "\""
+        os.system(string)
+        return 'have something to say?'
